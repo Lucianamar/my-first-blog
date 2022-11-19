2123f382c80c836d510c62b097d78984421a5eb1
Lucianamar <lumasantus04@gmail.com>
2022-11-19 17:07:58 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+from django.shortcuts import render
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
